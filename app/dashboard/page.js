f482912d4509b2ddcd5ea16b0fef4d73517a8ed2
Hanzla Tariq <thanzla2005@gmail.com>
2026-07-25 15:35:14 +0500

    one

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
-// pages/dashboard/page.jsx or app/dashboard/page.jsx
 import ChatDashboard from "@/components/chat/ChatDashboard";
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
